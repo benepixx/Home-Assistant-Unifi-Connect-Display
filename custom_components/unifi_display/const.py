5520cbfb4993ee661ecbaf212ee2ffa7c78cd1c8
Ben Tomlinson <53353052+benepixx@users.noreply.github.com>
2025-05-05 14:58:47 +0100

    Update const.py

--- custom_components/unifi_display/const.py
+++ custom_components/unifi_display/const.py
@@ -1,3 +1,5 @@
+# const.py - Constants and supported configuration
+
 DOMAIN = "unifi_display"
 NAME = "UniFi Display"
 MANUFACTURER = "Ubiquiti"
@@ -8,16 +10,44 @@ CONF_PASSWORD = "password"
 
 API_BASE = "/proxy/connect/api/v2"
 DEVICE_ENDPOINT = f"{API_BASE}/devices"
-STATUS_ENDPOINT = f"{API_BASE}/devices/{device_id}/status"
+STATUS_ENDPOINT = f"{API_BASE}/devices/{{device_id}}/status"  # Corrected line
 
 SUPPORTED_ACTIONS = [
-    "enable_auto_reload", "disable_auto_reload", "disable_auto_rotate",
-    "load_website", "refresh_website", "fw update", "reboot", "start_locating",
-    "stop_locating", "volume", "brightness", "play", "stop", "display_on",
-    "display_off", "enable_sleep", "disable_sleep", "enable_memorize_playlist",
-    "disable_memorize_playlist", "rotate", "enable_auto_rotate", "upgrade_mode",
-    "launch_app", "stop_app", "play_layout", "signage_screen_fit", "switch",
+    "enable_auto_reload",
+    "disable_auto_reload",
+    "disable_auto_rotate",
+    "load_website",
+    "refresh_website",
+    "fw update",
+    "reboot",
+    "start_locating",
+    "stop_locating",
+    "volume",
+    "brightness",
+    "play",
+    "stop",
+    "display_on",
+    "display_off",
+    "enable_sleep",
+    "disable_sleep",
+    "enable_memorize_playlist",
+    "disable_memorize_playlist",
+    "rotate",
+    "enable_auto_rotate",
+    "upgrade_mode",
+    "launch_app",
+    "stop_app",
+    "play_layout",
+    "signage_screen_fit",
+    "switch",
     "load_youtube"
+]
+
+AVAILABLE_APPS = [
+    "com.unifi.signage",
+    "com.google.android.youtube",
+    "com.android.chrome",
+    "com.ubnt.connect.home"
 ]
 
 SENSOR_TYPES = {
@@ -31,12 +61,34 @@ SENSOR_TYPES = {
 }
 
 BUTTON_ACTIONS = [
-    "display_on", "display_off", "reboot", "start_locating", "stop_locating",
-    "refresh_website", "upgrade_mode"
+    "display_on",
+    "display_off",
+    "reboot",
+    "start_locating",
+    "stop_locating",
+    "refresh_website",
+    "upgrade_mode"
 ]
 
 SWITCH_ACTIONS = [
-    "enable_auto_reload", "disable_auto_reload", "enable_sleep", "disable_sleep",
-    "enable_memorize_playlist", "disable_memorize_playlist", "enable_auto_rotate",
+    "enable_auto_reload",
+    "disable_auto_reload",
+    "enable_sleep",
+    "disable_sleep",
+    "enable_memorize_playlist",
+    "disable_memorize_playlist",
+    "enable_auto_rotate",
     "disable_auto_rotate"
 ]
+
+# Service constants for custom service calls
+SERVICE_LAUNCH_APP = "launch_app"
+SERVICE_LOAD_WEBSITE = "load_website"
+SERVICE_LOAD_YOUTUBE = "load_youtube"
+
+SERVICE_ARGUMENTS = {
+    SERVICE_LAUNCH_APP: ["app_id"],
+    SERVICE_LOAD_WEBSITE: ["url"],
+    SERVICE_LOAD_YOUTUBE: ["video_id"]
+}
+
